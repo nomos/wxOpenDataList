8ba268fb047ce470f2d07b9f3ffa8925120cdcf6
qomos <1201013@qq.com>
2018-09-19 11:59:11 +0800

    423

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ var WxOpenDataList = function () {
 
     this.canvas = wx.getSharedCanvas();
     this.ctx = this.canvas.getContext('2d');
-    ctx.fillRect(0,0,1000,1000);
+    this.ctx.width = 360;
+    this.ctx.height = 640;
+    this.ctx.fillRect(0,0,1000,1000);
     this.init();
 };
 
@@ -26,7 +28,6 @@ WxOpenDataList.prototype.fetchSelfInfo = function () {
         success: function(res) {
             console.log("fetchSelfCloudData success res=>", res);
             this.selfUserInfo = res.data[0];
-            this.listRenderer.setSelfInfo(res.data[0]);
         }.bind(this)
     });
 };
